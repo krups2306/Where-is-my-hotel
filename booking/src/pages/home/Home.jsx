ce6402d1a64d6312e1b03ef8eb1ad12512f19ab5
Krupa Bhavsar <krups4444@gmail.com>
2022-05-16 12:23:36 +0530

    navbar

--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import "./Home.css"
+import Navbar from "../../components/navbar/Navbar"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Navbar/>
+    </div>
   )
 }
 
