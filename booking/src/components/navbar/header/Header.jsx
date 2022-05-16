0b95117433130bab649d8cd8eccfac5a84f3c048
Krupa Bhavsar <krups4444@gmail.com>
2022-05-16 17:52:14 +0530

    header

--- booking/src/components/navbar/header/Header.jsx
+++ booking/src/components/navbar/header/Header.jsx
@@ -10,8 +10,9 @@ import "./Header.css"
 const Header = () => {
   return (
     <div className="header">
+      <div className="headerContainer">
         <div className="headerList">
-            <div className="headerListItems">
+            <div className="headerListItem active">
                 <FontAwesomeIcon icon={faBed} />
                 <span>Stays</span>
             </div>
@@ -32,6 +33,34 @@ const Header = () => {
                 <span>Airport taxis</span>
             </div>
         </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+              Get rewarded for your travels – unlock instant savings of 10% or
+              more with a free Lamabooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+            <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon"/>
+            <input 
+                type="text" 
+                placeholder="Where are you going?"
+                className="headerSearchInput"
+            />
+            </div>
+
+            <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
+            <span className="headerSearchText">date to date</span>
+            </div>
+
+            <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon"/>
+            <span className="headerSearchText">2 adults 2 children 1 room</span>
+            </div>
+
+        </div>
+      </div>
     </div>
   )
 }
