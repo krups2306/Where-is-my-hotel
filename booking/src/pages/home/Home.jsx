9c32fb91129b82eea8b36fcef141e7943be38d88
Krupa Bhavsar <krups4444@gmail.com>
2022-05-16 12:50:21 +0530

    header

--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css"
 import Navbar from "../../components/navbar/Navbar"
+import Header from "../../components/navbar/header/Header"
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Header/>
     </div>
   )
 }
